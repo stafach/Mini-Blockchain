c46117820d07081744c1436936f9fd0188c75862
chermat mustapha <mchermat@outlook.fr>
2026-02-27 11:18:02 +0100

    add the prototype of recreate_blockchain

--- back/blockchain/header/blockchain.h
+++ back/blockchain/header/blockchain.h
@@ -20,5 +20,5 @@ void mine_block(Block *block, int difficulty);
 Block *get_last_block(Blockchain *bc);
 void free_blockchain(Blockchain *bc);
 void print_blockchain(const Blockchain *bc);
-
+int recreate_blockchain(Blockchain *bc, int index, long timestamp, const char *data, const char *hash, const char *prev_hash, int nonce);
 #endif
