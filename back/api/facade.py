5a1c07b92354bfcc10f0582de922b4c10e1ad7ce
chermat mustapha <mchermat@outlook.fr>
2026-02-27 11:15:20 +0100

    create fonction for save the blockchain in a json file and an other for load this file, retrieving a new function in C

--- back/api/facade.py
+++ back/api/facade.py
@@ -1,5 +1,7 @@
 import ctypes
 from models import Block, Blockchain
+import json
+import os
 
 blockchain = ctypes.CDLL('/Users/chermatmustapha/Mini-Blockchain/back/blockchain/code/blockchain.so')
 
@@ -56,3 +58,69 @@ def print_full_blockchain(bc):
 
 # Verify is blockchain is active
 active_blockchain = None
+
+
+# Save the blockchain in a json file
+def save_blockchain(bc, filename="blockchain.json"):
+    chain_data = []
+    for i in range(bc.length):
+        b = bc.blocks[i].contents
+        chain_data.append({
+            "index": b.index,
+            "timestamp": b.timestamp,
+            "data": b.data.decode('utf-8'),
+            "hash": b.hash.decode('utf-8'),
+            "previous_hash": b.previous_hash.decode('utf-8'),
+            "nonce": b.nonce
+        })
+    
+    try:
+        with open(filename, 'w') as f:
+            json.dump(chain_data, f, indent=4)
+        print(f"Blockchain saved to {filename}")
+        return 0
+    except Exception:
+        return -1
+
+# Recreate
+recreate_bc = blockchain.recreate_blockchain
+recreate_bc.argtypes = [
+    ctypes.POINTER(Blockchain), 
+    ctypes.c_int, 
+    ctypes.c_long, 
+    ctypes.c_char_p, 
+    ctypes.c_char_p, 
+    ctypes.c_char_p, 
+    ctypes.c_int
+    ]
+recreate_bc.restype = ctypes.c_int
+
+# Recreate the blockchain with the json file
+def load_blockchain(bc, filename="blockchain.json"):
+    if not os.path.exists(filename):
+        return False
+    
+    with open(filename, 'r') as f:
+        data = json.load(f)
+    
+    if not data:
+        return False
+    
+    #clear the new blockchain
+    for i in range(bc.length):
+        free(bc.blocks[i])
+    bc.length = 0
+
+    for b in data:
+            recreate_bc(
+                ctypes.byref(bc),
+                b['index'],
+                b['timestamp'],
+                b['data'].encode('utf-8'),
+                b['hash'].encode('utf-8'),
+                b['previous_hash'].encode('utf-8'),
+                b['nonce']
+            )
+
+    print(f"Successfully restored {len(data)} blocks from {filename}")
+    return True
