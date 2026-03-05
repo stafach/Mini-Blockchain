ae5ccbb71f4aef489cdfad80a9f52628df13404a
chermat mustapha <mchermat@outlook.fr>
2026-03-05 12:51:57 +0100

    create class Transaction for the structure transaction and replaces data with transaction in Block

--- back/api/models.py
+++ back/api/models.py
@@ -1,14 +1,24 @@
 import ctypes
 
+class Transaction(ctypes.Structure):
+    _fields_ = [
+        ("sender", ctypes.c_char * 50),
+        ("receiver", ctypes.c_char * 50),
+        ("amount", ctypes.c_float)
+    ]
+
+
+
+
 class Block(ctypes.Structure):
     _fields_ = [
         ("index", ctypes.c_int),
         ("timestamp", ctypes.c_long),
-        ("data", ctypes.c_char * 256),  
+        ("tx_count", ctypes.c_int),
+        ("tx", Transaction * 5),  
         ("previous_hash", ctypes.c_char * 65), 
         ("hash", ctypes.c_char * 65),
-        ("nonce", ctypes.c_int),
-        ("prev", ctypes.c_void_p)               
+        ("nonce", ctypes.c_int)              
     ]
 
 class Blockchain(ctypes.Structure):
